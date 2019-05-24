b08a144a17f7096bd47cc459bdf902657feb5eec
drillprop <drylbartosz@gmail.com>
2019-05-24 11:44:45 +0200

    setup store and reducer with some random values for testing

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
-const store = {
+import { createStore } from 'react-redux';
+import reducer from '../reducers';
+
+const initialState = {
   key: 'value'
 };
+
+const store = createStore(reducer, initialState);
 
 export default store;
