eb4e488cf36d101d1763e83837d0e8656caf21fc
drillprop <drylbartosz@gmail.com>
2019-05-24 12:58:33 +0200

    create first reducer

--- src/duck/reducers.js
+++ src/duck/reducers.js
@@ -1,5 +1,25 @@
-const userReducer = state => {
-  return state;
+import { NICK, EMAIL, PASSWORD } from './types';
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case NICK:
+      return {
+        ...state,
+        nick: action.text
+      };
+    case EMAIL:
+      return {
+        ...state,
+        email: action.text
+      };
+    case PASSWORD:
+      return {
+        ...state,
+        password: action.text
+      };
+    default:
+      return state;
+  }
 };
 
 export default userReducer;
