ce8284ce8fd2798e2ab509c38c14a31fdea44200
drillprop <drylbartosz@gmail.com>
2019-05-24 17:44:42 +0200

    create mapDispatchToProps and add it to HOC

--- src/components/Forms/FormContainer.js
+++ src/components/Forms/FormContainer.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import AccountInfo from './AccountInfo';
+import { connect } from 'react-redux';
+import actions from '../../duck/actions';
 
-const FormContainer = () => {
+const FormContainer = props => {
+  console.log(props);
   return (
     <>
-      <AccountInfo />
+      <AccountInfo {...props} />
     </>
   );
 };
 
-export default FormContainer;
+const mapStateToProps = state => ({
+  nick: state.nick,
+  email: state.email,
+  password: state.password
+});
+
+const mapDispatchToProps = dispatch => ({
+  setNick: text => dispatch(actions.setNick(text)),
+  setEmail: text => dispatch(actions.setEmail(text)),
+  setPassword: text => dispatch(actions.setPassword(text))
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FormContainer);
