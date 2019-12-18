4623d5d6a2a6d29aba503c985f9e837c64c4a27e
drillprops <drylbartosz@gmail.com>
2019-12-18 18:47:39 +0100

    change theme color

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { GlobalStyle } from '../utils/GlobalStyle';
+import brown from '@material-ui/core/colors/brown';
 import Main from './Main';
 import Navigation from './Navigation';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: brown
+  }
+});
 
 const App = () => {
   return (
     <>
-      <Navigation />
-      <Main />
+      <ThemeProvider theme={theme}>
+        <Navigation />
+        <Main />
+      </ThemeProvider>
       <GlobalStyle />
     </>
   );
